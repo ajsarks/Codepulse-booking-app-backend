e0ab7edc9a05611b92256b174f17decc0dbd4ba3
ajsarks <aaribsarker@gmail.com>
2024-07-12 12:44:24 -0700

    Update verifytoken.js

--- backend/utils/verifytoken.js
+++ backend/utils/verifytoken.js
@@ -13,7 +13,15 @@ export const verifyToken = (req, res, next) => {
     next();
   });
 };
-
+export const verifyUserOrAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
 export const verifyUser = (req, res, next) => {
   verifyToken(req, res, next, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
